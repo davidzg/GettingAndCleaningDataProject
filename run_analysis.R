ba76e0a935d1f59a22e710783695f9df46024d50
davidzg <davidzapata88@gmail.com>
2015-06-17 00:24:18 +0300

    bckp

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,27 @@
 #setwd("E:/DATA_SCIENCE/3.GettingAndCleaningData/course_project")
 library(dplyr)
-trainFile <- "./data/UCI HAR Dataset/train/X_train.txt"
-testFile <- "./data/UCI HAR Dataset/test/X_test.txt"
-featuresFile <- "./data/UCI HAR Dataset/features.txt"
+
+msmTrainFile <- "./UCI HAR Dataset/train/X_train.txt"
+msmTestFile <- "./UCI HAR Dataset/test/X_test.txt"
+actTrainFile <- "./UCI HAR Dataset/train/y_train.txt"
+actTestFile <- "./UCI HAR Dataset/test/y_test.txt"
+sjtTrainFile <-"./UCI HAR Dataset/train/subject_train.txt"
+sjtTestFile <-"./UCI HAR Dataset/test/subject_test.txt"
+featuresFile <- "./UCI HAR Dataset/features.txt"
+
+
 features <- read.table(featuresFile)
-yActTest<-read.table("./data/UCI HAR Dataset/test/y_test.txt",col.names = "Activity")
-yActTrain<-read.table("./data/UCI HAR Dataset/train/y_train.txt",col.names = "Activity")
-#trainRaw <- tbl_df(read.table(trainFile, col.names = features$V2))
-#testRaw <- tbl_df(read.table(testFile, col.names = features$V2))
-trainRaw["Activity"]<- yActTrain[1]
-testRaw["Activity"]<- yActTest[1]
-dt<- rbind(trainRaw,testRaw)
-dt1<- select(dt,contains("mean"),contains("std"))
+actTest <- read.table(actTestFile,col.names = "Activity")
+actTrain <- read.table(actTrainFile,col.names = "Activity")
+sjtTest <- read.table(sjtTestFile,col.names = "Subject")
+sjtTrain <- read.table(sjtTrainFile,col.names = "Subject")
+if(!exists("msmTrain")) msmTrain <- read.table(msmTrainFile, col.names = features$V2)
+if(!exists("msmTest")) msmTest <- read.table(msmTestFile, col.names = features$V2)
+
+
+msmVariables <- grepl("-mean[^Ff]|-std()",features$V2)
+train <- cbind(sjtTrain,actTrain,msmTrain[msmVariables])
+test <- cbind(sjtTest,actTest,msmTest[grepl("-mean[^Ff]|-std()",features$V2)])
+dt <- tbl_df(rbind(train,test))
+
+
