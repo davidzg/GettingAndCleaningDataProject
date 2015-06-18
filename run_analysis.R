f66ec53e331447b71d80ccc471f27dfc09b8b723
davidzg <davidzapata88@gmail.com>
2015-06-18 04:03:30 +0300

    backp

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 #setwd("E:/DATA_SCIENCE/3.GettingAndCleaningData/course_project")
-library(dplyr)
+library(reshape2)
 
 msmTrainFile <- "./UCI HAR Dataset/train/X_train.txt"
 msmTestFile <- "./UCI HAR Dataset/test/X_test.txt"
@@ -18,10 +18,25 @@ sjtTrain <- read.table(sjtTrainFile,col.names = "Subject")
 if(!exists("msmTrain")) msmTrain <- read.table(msmTrainFile, col.names = features$V2)
 if(!exists("msmTest")) msmTest <- read.table(msmTestFile, col.names = features$V2)
 
-
+#Step1 and Step2
 msmVariables <- grepl("-mean[^Ff]|-std()",features$V2)
 train <- cbind(sjtTrain,actTrain,msmTrain[msmVariables])
 test <- cbind(sjtTest,actTest,msmTest[msmVariables])
-dt <- tbl_df(rbind(train,test))
+dt <- data.frame(rbind(train,test))
 
+#Step 3
+avtivityLabel <- c("Walking", "Walking_Upstairs", "Walking_Downstairs",
+                   "Sitting", "Standing", "Laying")
+dt$Activity<- factor(dt$Activity,labels = avtivityLabel)
 
+#Step 4
+labelAppropiator<-function(x){
+  x<-gsub(".mean..","Mean",x)
+  x<-gsub(".std..","Std",x)
+  x<-gsub(".([XYZ])","\\1",x)
+
+}
+names(dt)<-sapply(names(dt),labelAppropiator)
+
+#Step5
+tidyDF<-aggregate(dt[3:68],list(Subject=dt$Subject,Activity=dt$Activity),mean)
